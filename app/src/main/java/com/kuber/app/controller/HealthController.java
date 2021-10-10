9244e2a6370b7c4376e67c6733f0a2eb110ba1b4
Nitesh Nandan <nitesh.nandan@meesho.com>
2021-10-10 22:26:18 +0530

    just donde:

--- app/src/main/java/com/kuber/app/controller/HealthController.java
+++ app/src/main/java/com/kuber/app/controller/HealthController.java
@@ -1,17 +1,20 @@
 package com.kuber.app.controller;
 
 import com.kuber.common.constants.APIPath;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HealthController {
 
     @GetMapping(value = APIPath.HEALTH_CHECK)
     @ResponseStatus(HttpStatus.OK)
     public String healthCheck() {
+        log.info("Test");
         return "success";
     }
 }
